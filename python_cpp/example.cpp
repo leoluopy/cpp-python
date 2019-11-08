fdd99ab3021c7ad10affbd02565ae068c0023ad7
leo_luopy@139.com <lpy1990fff>
2019-11-08 16:00:24 +0800

    python cpp

--- python_cpp/example.cpp
+++ python_cpp/example.cpp
@@ -30,6 +30,30 @@ py::array_t<unsigned char> pass_string_return_mat(char *str) {
     return k;
 }
 
+int mat_show_in_cv(py::array_t<unsigned char> &img) {
+    if (img.ndim() != 3) {
+        throw std::runtime_error("RGB image must has 3 channels!");
+    } else {
+        printf(" dim check passed \n");
+    }
+    printf("input shape: %d %d %d", img.shape()[0], img.shape()[1], img.shape()[2]);
+    auto imgNumpy = img.unchecked<3>();
+
+    cv::Mat im = cv::Mat(img.shape()[0], img.shape()[1], CV_8UC3);
+    for (int row = 0; row < im.rows; row++) {
+        for (int col = 0; col < im.cols; col++) {
+            im.at<cv::Vec3b>(row, col)[0] = static_cast<int>(imgNumpy(row, col, 0));
+            im.at<cv::Vec3b>(row, col)[1] = static_cast<int>(imgNumpy(row, col, 1));
+            im.at<cv::Vec3b>(row, col)[2] = static_cast<int>(imgNumpy(row, col, 2));
+//            printf("%d \n", im.at<cv::Vec3b>(row, col)[0]);
+        }
+    }
+    cv::imshow("xxxx", im);
+    cv::waitKey(0);
+
+
+    return 88;
+}
 
 PYBIND11_MODULE(example, m) {
     // optional module docstring
@@ -37,7 +61,10 @@ PYBIND11_MODULE(example, m) {
     // expose add function, and add keyword arguments and default arguments
     m.def("add", &add, "A function which adds two numbers", py::arg("i") = 1, py::arg("j") = 2);
     m.def("pass_string", &pass_string, "A function which pass string", py::arg("str") = "expstring");
-    m.def("pass_string_return_mat", &pass_string_return_mat, "A function which pass string return mat", py::arg("str") = "expstring");
+    m.def("pass_string_return_mat", &pass_string_return_mat, "A function which pass string return mat",
+          py::arg("str") = "expstring");
+    m.def("mat_show_in_cv", &mat_show_in_cv, "A function which pass mat and show");
+
 
     // exporting variables
     m.attr("the_answer") = 42;
--- python_cpp/exp.py
+++ python_cpp/exp.py
@@ -1,16 +1,15 @@
-
-
 import example
+import cv2
 
 ret = example.add(1, 2)
 print(ret)
 
-
 ret = example.pass_string("../test")
 print(ret)
 
+ims = cv2.imread("./1.png")
+example.mat_show_in_cv(ims)
+
 ret = example.pass_string_return_mat("./1.png")
-print (ret)
+print(ret)
 print(ret.shape)
-
-
